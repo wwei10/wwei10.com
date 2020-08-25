6d6fc74a43886f3ade77fee0336d5c8d681ad250
Wei Wei <weiweisjtu@gmail.com>
2020-08-25 16:40:58 -0700

    Change deploy.sh to specify go binary location

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,7 @@
-go install && sudo systemctl stop blog && sudo systemctl start blog
+#!/usr/bin/bash
+
+if [ -f /usr/local/go/bin/go  ]
+then
+
+  /usr/local/go/bin/go install && sudo systemctl stop blog && sudo systemctl start blog
+fi
