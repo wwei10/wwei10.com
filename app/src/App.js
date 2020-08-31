8694b6dc9b97ee0d377f77bcb60afcc3198f089a
Wei Wei <weiweisjtu@gmail.com>
2020-08-30 20:29:27 -0700

    Fix routing

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,38 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useLocation,
+} from 'react-router-dom';
 
 import Header from './components/Header';
 import About from './components/About';
 import Feed from './components/Feed';
 import Post from './components/Post';
 
+function NoMatch() {
+  let location = useLocation();
+
+  return (
+    <div class="wrap yue">
+      <h3>
+        No match for <code>{location.pathname}</code>
+      </h3>
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
       <Header />
-      <Route path="/posts/:permalink" component={Post} />
-      <Route path="/about" exact component={About} />
-      <Route path="/" exact component={Feed} />
+      <Switch>
+        <Route path="/posts/:permalink" component={Post} />
+        <Route exact path="/about" exact component={About} />
+        <Route exact path="/" component={Feed} />
+        <Route path="*" component={NoMatch} />
+      </Switch>
     </Router>
   );
 }
