818cc5cba1fceb6b237f53231072faa09aa3e34b
Wei Wei <weiweisjtu@gmail.com>
2020-09-07 13:00:23 -0700

    Introduce more space between page view counter and comment section

--- app/src/components/Post/index.js
+++ app/src/components/Post/index.js
@@ -65,6 +65,7 @@ class Post extends React.Component {
                         </div>
                         <div id='discourse-comments'></div>
                     </div>
+                    <br />
                     <div className="wrap yue">
                         <span style={{ display: 'inline', float: 'left' }}>
                             <small>本页面访问量{ this.state.postView }次</small>
